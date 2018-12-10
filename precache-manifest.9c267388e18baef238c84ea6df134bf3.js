c6206e0547213741fac89db7abb89275aa26ef4c
marcosrib <marcos.rib.sousa@gmail.com>
2018-12-10 21:11:20 -0200

    first commit

---
+++ precache-manifest.9c267388e18baef238c84ea6df134bf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d389ab7d5928b7ed5c77",
+    "url": "/static/js/main.d389ab7d.chunk.js"
+  },
+  {
+    "revision": "b37a910bd64f1f124b65",
+    "url": "/static/js/1.b37a910b.chunk.js"
+  },
+  {
+    "revision": "d389ab7d5928b7ed5c77",
+    "url": "/static/css/main.7183514e.chunk.css"
+  },
+  {
+    "revision": "d781945989355c24faa0799d60939fa8",
+    "url": "/index.html"
+  }
+];
